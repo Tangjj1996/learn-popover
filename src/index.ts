4a7c32ae9733bab30efed88d937a7243e6eafc70
Tangjj1996 <tangjj_1996@163.com>
2021-07-23 17:03:35 +0800

    feat: add hot module replacement plugin.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
 console.log('this is really a test')
+console.log('this is another file')
--- webpack/dev.config.ts
+++ webpack/dev.config.ts
@@ -1,10 +1,16 @@
-import { webpack } from 'webpack'
+import { webpack, HotModuleReplacementPlugin } from 'webpack'
 import merge from 'webpack-merge'
 import baseConfig from './base.config'
 import HtmlWebpackPlugin from 'html-webpack-plugin'
 import path from 'path'
 import chalk from 'chalk'
-const log = console.log
+import { log, error } from './utils'
+import * as fs from 'fs'
+
+fs.rmSync(path.resolve(__dirname, '../build'), {
+  recursive: true,
+  force: true,
+})
 
 const devOptions = merge(baseConfig, {
   mode: 'development',
@@ -13,19 +19,20 @@ const devOptions = merge(baseConfig, {
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, '../public/index.html'),
     }),
+    new HotModuleReplacementPlugin(),
   ],
 })
 const complier = webpack(devOptions)
 
-const watching = complier.watch({}, (err, stats) => {
+complier.run((err, stats) => {
   if (err || stats.hasErrors()) {
-    log(chalk.red(stats.toString()))
+    error(chalk.red(stats.toString()))
     process.exit(1)
   }
-})
 
-watching.close((err) => {
-  if (err) {
-    log(chalk.red(err))
-  }
+  log(
+    stats.toString({
+      color: true,
+    })
+  )
 })
